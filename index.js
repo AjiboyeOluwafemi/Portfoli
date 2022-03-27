67c8324d11e5141b4d798a76692aa70085d38e19
Kayode <87308410+AjiboyeOluwafemi@users.noreply.github.com>
2022-03-27 16:04:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let sideToggle = true;
 sideBtnClick = ()=> {
-    const getSideBtn = document.querySelector("#sideBar");
+    const getSideBar = document.querySelector("#sideBar");
     const getImg = document.getElementById("myImg");
 
     if (!!sideToggle) {
-        getSideBtn.style.display = "unset";
-        getSideBtn.classList.add("animate__fadeInRight");
-        getSideBtn.classList.remove("animate__fadeOutRight");
+        getSideBar.style.display = "unset";
+        getSideBar.classList.add("animate__fadeInRight");
+        getSideBar.classList.remove("animate__fadeOutRight");
 
         setTimeout(() => {
             getImg.src = "images/svg/close.svg";
@@ -15,11 +15,10 @@ sideBtnClick = ()=> {
         sideToggle = false;
     } else if (!sideToggle) {
         
-        getSideBtn.classList.remove("animate__fadeInRight");
-        getSideBtn.classList.add("animate__fadeOutRight");
+        getSideBar.classList.remove("animate__fadeInRight");
+        getSideBar.classList.add("animate__fadeOutRight");
         setTimeout(() => {
             getImg.src = "images/svg/dots.svg";
-            getSideBtn.style.display = "none";
         }, 200);
         
         sideToggle = true;
