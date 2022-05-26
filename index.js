bbd1b0197826421a5c88aa639221b8cb04400d47
Kayode <87308410+AjiboyeOluwafemi@users.noreply.github.com>
2022-05-26 21:39:12 +0100

    update

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-let sideToggle = true;
-sideBtnClick = ()=> {
-    const getSideBar = document.querySelector("#sideBar");
-    const getImg = document.getElementById("myImg");
-
-    if (!!sideToggle) {
-        getSideBar.style.display = "unset";
-        getSideBar.classList.add("animate__fadeInRight");
-        getSideBar.classList.remove("animate__fadeOutRight");
-
-        setTimeout(() => {
-            getImg.src = "images/svg/close.svg";
-            getSideBar.style.position = "absolute";
-        }, 1000);
-
-        sideToggle = false;
-    } else if (!sideToggle) {
-        getSideBar.classList.remove("animate__fadeInRight");
-        getSideBar.classList.add("animate__fadeOutRight");
-        getSideBar.style.position = "fixed";
-
-        setTimeout(() => {
-            getImg.src = "images/svg/dots.svg";
-        }, 500);
-        
-        sideToggle = true;
-    } 
-}
-
-
-
-
 const spinner = document.querySelector("#siteSpinner");
 const content = document.querySelector("#siteContent");
 setTimeout(() => {
